8d55ee02b80d9cdbc16b98c0004de8548e20cf9b
erascardsilva <erasmo@vmi-informatica.com.br>
2026-04-05 15:00:37 -0300

    feat: update professional profile and add new projects to portfolio pages

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Erasmo Cardoso | Software Engineer | Electronics Specialist';
-export const SITE_DESCRIPTION = 'Portfólio profissional de Erasmo Cardoso, Engenheiro de Software e Especialista em Eletrônica.';
+export const SITE_DESCRIPTION = 'Sistemas SaaS, Backend (Node/Go/Docker) e 30+ anos de expertise em Eletrônica.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
