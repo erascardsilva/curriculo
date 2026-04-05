be996b588d37b66e74d2edc989f72ef8e619f62c
erascardsilva <erasmo@vmi-informatica.com.br>
2026-04-05 14:57:12 -0300

    refactor: update professional profile description and project details in index page and site configuration

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Erasmo Cardoso | Portfólio';
-export const SITE_DESCRIPTION = 'Seja bem vindo ao meu portfólio.';
+export const SITE_TITLE = 'Erasmo Cardoso | Software Engineer | Electronics Specialist';
+export const SITE_DESCRIPTION = 'Portfólio profissional de Erasmo Cardoso, Engenheiro de Software e Especialista em Eletrônica.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
